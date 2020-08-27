da9772f21d76f757fa15a0897a9336b799bf8653
tony-s-xiao <tony.s.xiao@gmail.com>
2020-08-26 20:20:43 -0700

    making parser

--- SimplestDb/simplestdb.cpp
+++ SimplestDb/simplestdb.cpp
@@ -1,11 +1,54 @@
 #include<iostream>
+#include<string>
+
 
 using namespace std;
 
 int main()
 {
+//loop to take in inputs
 
-	cout << "hello wow" << endl;
+	string commandInput = "";
+	string exit = ".exit"; //whats the best way to check for these commands?
+	
+	while (commandInput != exit)
+	{
+
+		cout << "Enter Command: " << endl;
+		cin >> commandInput;
+		cout << commandInput;
+		
+
+	}
 
 	return 0;
 }
+
+class Parser
+{
+
+	public:
+		Parser() : data("")
+		{
+			
+		}
+		Parser(string command)
+		{
+			data = command;
+		}
+		~Parser() 
+		{
+
+		}
+
+	protected:
+
+	private:
+
+		string data;
+
+
+
+
+
+};
