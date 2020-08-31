331a8369f6d172b8bf8f09e718a347a5fe82b0a7
tony-s-xiao <tony.s.xiao@gmail.com>
2020-08-30 20:07:00 -0700

    parser

--- SimplestDb/simplestdb.cpp
+++ SimplestDb/simplestdb.cpp
@@ -1,54 +1,81 @@
 #include<iostream>
 #include<string>
-
+#include<vector>
+#include<unordered_map>
 
 using namespace std;
 
-int main()
-{
-//loop to take in inputs
+enum Command{INSERT=1, QUERY, EXIT, NONE};
 
-	string commandInput = "";
-	string exit = ".exit"; //whats the best way to check for these commands?
-	
-	while (commandInput != exit)
+class Parser
+{
+
+public:
+	Parser() : data("")
 	{
 
-		cout << "Enter Command: " << endl;
-		cin >> commandInput;
-		cout << commandInput;
-		
+	}
 
+	Parser(string command)
+	{
+		int endSpace = command.find_first_of(" ");
+		data = command.substr(endSpace+1);
+
+		auto currEle = lookupCommandTable.find(command.substr(0, endSpace));
+
+		if (currEle != lookupCommandTable.end())
+			commandType = lookupCommandTable.find(command.substr(0, endSpace))->second;
+		else
+			commandType = NONE;
+
+
+	}
+
+	~Parser()
+	{
+
+	}
+
+	Command getCommand()
+	{
+		return commandType;
+	}
+
+	bool exit()
+	{
+		return commandType == EXIT ? true : false;
+	}
+
+protected:
+
+private:
+
+	string data;
+	Command commandType;
+	static unordered_map<string, Command> lookupCommandTable;
+
+};
+
+unordered_map<string, Command> Parser::lookupCommandTable = { {"insert", INSERT}, {".exit", EXIT}, {"select", QUERY} };
+
+
+int main()
+{
+
+	string commandInput = "";
+
+	cout << "Enter Command: ";
+	getline(cin, commandInput);
+	Parser currCommand = Parser(commandInput);
+	
+	while(true)
+	{
+		cout << currCommand.getCommand();
+		cout << "Enter Command: ";
+		getline(cin, commandInput);
+		currCommand = Parser(commandInput);
 	}
 
 	return 0;
 }
 
-class Parser
-{
-
-	public:
-		Parser() : data("")
-		{
-			
-		}
-		Parser(string command)
-		{
-			data = command;
-		}
-		~Parser() 
-		{
-
-		}
-
-	protected:
-
-	private:
-
-		string data;
-
-
-
-
-
-};
